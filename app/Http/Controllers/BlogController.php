4fc46a63c2c686fa88011f843bcaeff4980a96e3
[shuraz] <mrsurajpokhrel@gmail.com>
2021-07-10 23:13:10 +1000

    List added and delete in database

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Restaurant;
 
 class BlogController extends Controller
 {
@@ -10,6 +11,28 @@ class BlogController extends Controller
         return view('home');
     }
     public function list(){
-        return view('list');
+        $data= Restaurant::all();
+        return view('list',['data'=>$data]); 
+    }
+    function add(Request $req){
+        //return $req->input();
+        $restro= new Restaurant;
+        $restro->name=$req->input('name');
+        $restro->email=$req->input('email');
+        $restro->address=$req->input('address');
+        $restro->save();
+        $req->session()->flash('status', 'Restaurant entered successfully!!!');  
+        return redirect('list');
+    }
+    
+    function delete($id){
+        Restaurant::find($id)->delete();
+       // Session::flash('status','Restaurant has successfully deleted!!!');
+        return redirect('list');
+    }
+
+    function edit($id){
+        return Restaurant::find($id);
     }
 }
+
