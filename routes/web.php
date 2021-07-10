4916b1d7ddc9d656cdaefacb4fbe55ced55249a4
[shuraz] <mrsurajpokhrel@gmail.com>
2021-07-10 15:47:06 +1000

    home page, layout and bootstrap setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BlogController::class, 'index']);
+//Route::view('home', 'layout');
